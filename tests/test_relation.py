b7055b94325b78214d98512931aae2113a37265a
Eric Mercer <egm@cs.byu.edu>
2025-10-21 17:48:45 +0000

    Add tests for union and project from Jupyter Notebook

--- tests/test_relation.py
+++ tests/test_relation.py
@@ -139,3 +139,147 @@ def test_given_matched_relations_when_difference_then_difference():
 
     # then
     assert expected == answer
+
+
+def test_union_raises_when_headers_differ() -> None:
+    # given
+    P = Relation(["bart", "lisa"], {("h", "1"), ("m", "2")})
+    W = Relation(["bart", "maggie"], {("f", "3")})  # header differs in second column
+
+    # when
+    # then
+    with pytest.raises(
+        IncompatibleOperandError,
+        match=r"Error: ('h', 1) is not type compatible with Relation.RelationTuple in Relation.add_tuple",
+    ):
+        P.union(W)
+
+
+@pytest.mark.parametrize(
+    argnames=("hdr, rows1, rows2, expected_rows"),
+    argvalues=(
+        # Disjoint
+        (
+            ["A", "B"],
+            {("1", "a"), ("2", "b")},
+            {("3", "c")},
+            {("1", "a"), ("2", "b"), ("3", "c")},
+        ),
+        # Overlap (duplicates should be removed by set semantics)
+        (
+            ["A", "B"],
+            {("1", "a"), ("2", "b")},
+            {("2", "b"), ("3", "c")},
+            {("1", "a"), ("2", "b"), ("3", "c")},
+        ),
+        # Left empty
+        (["A", "B"], set(), {("1", "a")}, {("1", "a")}),
+        # Right empty
+        (["A", "B"], {("1", "a")}, set(), {("1", "a")}),
+        # Idempotent: R ∪ R = R
+        (
+            ["A", "B"],
+            {("10", "x"), ("11", "y")},
+            {("10", "x"), ("11", "y")},
+            {("10", "x"), ("11", "y")},
+        ),
+    ),
+    ids=("Disjoint", "Overlap", "Left Empty", "Right Empty", "Idempotent"),
+)
+def test_union_basic_cases(hdr, rows1, rows2, expected_rows):
+    # given
+    r1 = Relation(hdr, rows1)
+    r2 = Relation(hdr, rows2)
+    expected = Relation(hdr, expected_rows)
+
+    # when
+    answer = r1.union(r2)
+
+    # then
+    assert expected == answer
+
+
+def test_given_bad_col_when_project_then_raise() -> None:
+    # given
+    r = Relation(["A", "B"], {("x", "1")})
+    project_hdr = ["Z"]
+
+    # when
+    # then
+    with pytest.raises(IncompatibleOperandError):
+        r.project(project_hdr)
+
+
+@pytest.mark.parametrize(
+    argnames=("hdr, rows, project_hdr, expected_rows"),
+    argvalues=(
+        # Disjoint
+        (
+            ["A", "B"],
+            set(),
+            ["A"],
+            set(),
+        ),
+        # Two Columns
+        (
+            ["A", "B"],
+            {("x", "1"), ("y", "2")},
+            ["A"],
+            {("x",), ("y",)},
+        ),
+        # Two Columns with Duplicates
+        (
+            ["A", "B"],
+            {("x", "1"), ("x", "2")},
+            ["A"],
+            {("x",)},
+        ),
+        # Three Columns
+        (
+            ["A", "B", "C"],
+            {("x1", "True"), ("y", "2", "False")},
+            ["B"],
+            {(1,), (2,)},
+        ),
+        # Three Columns with Two Adjacent
+        (
+            ["A", "B", "C"],
+            {
+                ("x", "1", "True"),
+                ("y", "2", "False"),
+                ("x", "1", "False"),
+            },
+            ["A", "B"],
+            {("x", "1"), ("y", "2")},
+        ),
+        # Three Columns with Two Not Adjacent
+        (
+            ["A", "B", "C"],
+            {
+                ("x", "1", "True"),
+                ("y", "2", "False"),
+                ("x", "9", "True"),
+            },
+            ["A", "C"],
+            {("x", "True"), ("y", "True")},
+        ),
+    ),
+    ids=(
+        "Empty",
+        "Two Cols",
+        "Two Cols with Dups",
+        "Three Cols",
+        "Three Cols with Two Adj",
+        "Three Cols with Two Not Adj",
+    ),
+)
+def test_project_basic_cases(hdr, rows, project_hdr, expected_rows):
+    # given
+    r = Relation(hdr, rows)
+    expected = Relation(project_hdr, expected_rows)
+
+    # when
+    answer = r.project(project_hdr)
+
+    # then
+    assert expected == answer
